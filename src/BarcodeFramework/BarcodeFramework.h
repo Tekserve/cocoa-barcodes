357e6f5d7794e1194b4f52da85baf33bf9762c2d
Jeremy Agostino <jeremy@tekserve.com>
2013-02-13 18:53:40 -0500

    Created proper public framework interface header.

--- src/BarcodeFramework/BarcodeFramework.h
+++ src/BarcodeFramework/BarcodeFramework.h
@@ -8,3 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+#import <BarcodeFramework/NKDBarcode.h>
+#import <BarcodeFramework/NKDBarcodeOffscreenView.h>
+#import <BarcodeFramework/NKDCode39Barcode.h>
+#import <BarcodeFramework/NKDExtendedCode39Barcode.h>
+#import <BarcodeFramework/NKDInterleavedTwoOfFiveBarcode.h>
+#import <BarcodeFramework/NKDModifiedPlesseyBarcode.h>
+#import <BarcodeFramework/NKDPostnetBarcode.h>
+#import <BarcodeFramework/NKDUPCABarcode.h>
+#import <BarcodeFramework/NKDModifiedPlesseyHexBarcode.h>
+#import <BarcodeFramework/NKDIndustrialTwoOfFiveBarcode.h>
+#import <BarcodeFramework/NKDEAN13Barcode.h>
+#import <BarcodeFramework/NKDCode128Barcode.h>
+#import <BarcodeFramework/NKDCodabarBarcode.h>
+#import <BarcodeFramework/NSImage-NKDBarcode.h>
+#import <BarcodeFramework/NSImage-Resolution.h>
+#import <BarcodeFramework/NKDUPCEBarcode.h>
+#import <BarcodeFramework/NKDEAN8Barcode.h>
+#import <BarcodeFramework/NKDRoyalMailBarcode.h>
+#import <BarcodeFramework/NKDPlanetBarcode.h>
+#import <BarcodeFramework/JapanPostBarcode.h>
